c40f3dd454f14c69a3b083d65d67df96a2fc5bfb
serafim.eth <serafimcloud@gmail.com>
2025-02-25 16:01:51 +0300

    init

--- src/utils/http-client.ts
+++ src/utils/http-client.ts
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 const TWENTY_FIRST_API_KEY = process.env.TWENTY_FIRST_API_KEY || "test";
+const TWENTY_FIRST_API_URL =
+  process.env.TWENTY_FIRST_API_URL || "https://magic.21st.dev";
 
 if (!TWENTY_FIRST_API_KEY) {
   throw new Error("TWENTY_FIRST_API_KEY environment variable is not set");
 }
 
 export const twentyFirstClient = axios.create({
-  baseURL: "https://magic.21st.dev",
+  baseURL: TWENTY_FIRST_API_URL,
   headers: {
     "x-api-key": TWENTY_FIRST_API_KEY,
     "Content-Type": "application/json",
